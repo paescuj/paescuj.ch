270d209ff3618e4ee75e79fff7ef58e52a28dee3
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-05-26 11:19:07 +0200

    Update vite, inline css into index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import { minifyHtml } from 'vite-plugin-html';
 
 export default defineConfig({
   plugins: [
+    viteSingleFile(),
     minifyHtml(),
   ],
 });
